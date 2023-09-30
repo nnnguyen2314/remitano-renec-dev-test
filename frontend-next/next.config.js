9d72ba02ce4aa5847b879f8411cdba7e2bb4baab
nnnguyen <nhatnguyen.nguyen23@gmail.com>
2023-09-30 14:18:36 +0700

    Fixed Eslint errors

--- frontend-next/next.config.js
+++ frontend-next/next.config.js
@@ -3,6 +3,11 @@ const webpack = require('webpack');
 const withLess = require('next-with-less');
 const lessToJS = require('less-vars-to-js');
 
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+        return config;
+    }
+}
 
 module.exports = nextConfig
