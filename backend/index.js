f43cb62e6b74ea5f502a54a790281cb0a4fc1433
nnnguyen <nhatnguyen.nguyen23@gmail.com>
2023-09-30 15:02:55 +0700

    Add vercel config for Backend deployment

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const authAPIRouters = require('./src/routes/auth');
+const videoAPIRouters = require('./src/routes/video');
+
+const authRoute = express.Router();
+const videoRoute = express.Router();
+
+global.__basedir = __dirname;
+const port = process.env.PORT || 3100;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+authRoute.post('/api/checkAuth', authAPIRouters.doAuthCheck);
+authRoute.post('/api/auth', authAPIRouters.doAuth);
+authRoute.get('/api/profile', authAPIRouters.doAuthCheck, authAPIRouters.fetchProfile);
+
+videoRoute.get('/api/videos', videoAPIRouters.fetchVideos);
+videoRoute.get('/api/videoInfo/:videoUrl', videoAPIRouters.fetchVideoInfo);
+videoRoute.post('/api/video', authAPIRouters.doAuthCheck, videoAPIRouters.postVideo);
+
+app.use(authRoute);
+app.use(videoRoute);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+module.exports = app;
